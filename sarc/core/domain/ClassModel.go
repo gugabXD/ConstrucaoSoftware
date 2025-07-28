8062c756318c7f7e52ec96a4048663d71fb1f090
gugab <gustavobpasquali2@gmail.com>
2025-07-27 21:40:09 -0300

    remove old sarc folder

--- sarc/core/domain/ClassModel.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type Class struct {
-	ClassID      uint   `gorm:"primaryKey" json:"classId"`
-	Name         string `json:"name"`
-	Description  string `json:"description"`
-	DisciplineID uint   `json:"disciplineId"`
-}
--- sarc/core/domain/ReservationModel.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type Reservation struct {
-	ReservationID uint       `gorm:"primaryKey" json:"reservationId"`
-	LectureID     uint       `json:"lectureId"`
-	Observation   string     `json:"observation"`
-	Resources     []Resource `gorm:"many2many:reservation_resources;" json:"resources"`
-}
